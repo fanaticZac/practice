d5776521d5ac0a3f18376803271f4b9c3397f0f6
zacharySee <zachary.jk.carlson@gmail.com>
2018-07-24 04:50:10 -0700

    Created practice console app

---
+++ practice0/practice0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I created project, then put a shortcut in the main folder.");
+
+            Console.Read();
+        }
+    }
+}
